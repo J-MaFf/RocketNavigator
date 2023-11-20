221e3c8791796605694aa1669aab7f9465680b8d
Jesus Olvera-Lara <51460948+AzulDusk80@users.noreply.github.com>
2023-11-19 18:22:39 -0600

    Create Altimeter.py
    added altimeter, basic idea for i2c

---
+++ src/Altimeter.py
@@ -0,0 +1,24 @@
+import board
+import adafruit_bmp3xx
+#https://docs.circuitpython.org/projects/bmp3xx/en/latest/api.html#adafruit_bmp3xx.BMP3XX.altitude
+
+def getTempt():
+  temperature = bmp.temperature
+  pressure = bmp.pressure
+  altitude = bmp.altitude
+  return temperature
+
+def getPres():
+  pressure = bmp.pressure
+  return pressure
+
+def getAltit():
+  altitude = bmp.altitude
+  return altitude
+
+
+i2c = board.I2C()  
+bmp = adafruit_bmp3xx.BMP3XX_I2C(i2c)
+temperature = bmp.temperature
+pressure = bmp.pressure
+altitude = bmp.altitude
