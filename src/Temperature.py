0281fb88b9e22fe9dd5606786f24fd4d3f296470
Jesus Olvera-Lara <51460948+AzulDusk80@users.noreply.github.com>
2023-11-21 17:12:03 -0600

    Create Temperature.py
    get temperature for tmp36

---
+++ src/Temperature.py
@@ -0,0 +1,7 @@
+import board
+import analogio
+
+def getTemp(analogin):
+    millivolts = analogin.value * (analogin.reference_voltage * 1000 / 65535)
+    temp_C = (millivolts - 500) / 10
+    temp_F = (temp_C * 9/5) + 32
