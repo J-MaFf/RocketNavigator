cc0625955db199459dfebf3a7b6ce7ab193c3c78
Jesus Olvera-Lara <51460948+AzulDusk80@users.noreply.github.com>
2023-11-27 21:23:05 -0600

    Create GPS.py

---
+++ src/GPS.py
@@ -0,0 +1,15 @@
+import gnss
+
+#https://content.u-blox.com/sites/default/files/products/documents/GNSS-Antennas_AppNote_%28UBX-15030289%29.pdf
+#https://docs.circuitpython.org/en/8.2.x/shared-bindings/gnss/index.html#
+
+def getLat():
+  return format(nav.latitude)
+
+def getLong():
+  return format(nav.longitude)
+  
+def getUpdate():
+  nav.update()
+
+locate = gnss.GNSS([gnss.SatelliteSystem.GPS, gnss.SatelliteSystem.GLONASS])
