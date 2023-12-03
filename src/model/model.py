59a1b46d30bb89d3a1732c4e5e14dceab8c97e17
Jesus Olvera-Lara <51460948+AzulDusk80@users.noreply.github.com>
2023-12-02 22:28:35 -0600

    Update model.py
    temp add to model

--- src/model/model.py
+++ src/model/model.py
@@ -79,6 +79,12 @@ class TemperatureModel(SensorModel):
 
     def __init__(self, pin):
         super().__init__(pin)
+        os.system("modprobe w1-gpio")
+        os.system("modprobe w1-therm")
+        
+        base_dir = "/sys/bus/w1/devices/"
+        device_folder = glob.glob(base_dir + "28*")[0]
+        device_file = device_folder + "/w1_slave"
 
     def readData(self):
         """
@@ -88,7 +94,13 @@ class TemperatureModel(SensorModel):
             float: The temperature in Fahrenheit.
         """
         sensorValue = GPIO.input(self.pin)
-        return self.convertData(sensorValue)
+        return self.convertData()
+
+    def read_temp_raw():
+        f = open(device_file, "r")
+        lines = f.readlines()
+        f.close()
+        return lines
 
     def convertData(sensorValue):
         """
@@ -100,7 +112,17 @@ class TemperatureModel(SensorModel):
         Returns:
             float: The temperature in Fahrenheit.
         """
-        return sensorValue * 1.8 + 32  # CHECK CONVERSION FORMULA
+        lines = read_temp_raw()
+        while lines[0].strip()[-3:] != "YES":
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find("t=")
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos + 2 :]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            return temp_f
+        #return sensorValue * 1.8 + 32  # CHECK CONVERSION FORMULA
 
 
 class Altimeter(SensorModel):
