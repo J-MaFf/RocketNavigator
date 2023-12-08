9973aa0d30bf62a1aa317889897122e8904618cb
Jesus Olvera-Lara <51460948+AzulDusk80@users.noreply.github.com>
2023-12-08 08:58:02 -0600

    Update view.py

--- src/view/view.py
+++ src/view/view.py
@@ -6,15 +6,16 @@ class RocketView:
     # TODO: Add a method to display the data
     def display_data(self, data):
         # Print the data or update the display
-        if isinstance(data, str):
-            print(data)
-        elif isinstance(data, list):
+        if isinstance(data, list):
             print(", ".join(data))
+        else:
+            print(data)
 
     # TODO: Add a method to wirte the data to a csv file
     def write_data(self, data):
         with open(csv_file_name, mode=w) as file:
-            if isinstance(data, str):
-                file.write(data + ",")
-            elif isinstance(data, list):
+            # Adds data to file
+            if isinstance(data, list):
                 file.write(", ".join(data))
+            else:
+                file.write(data + ",")
