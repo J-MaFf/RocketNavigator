d2df216d77c4c7ed42f007b3b182a896eeb9922e
NickofTime6 <130617638+NickofTime6@users.noreply.github.com>
2023-12-12 17:50:27 -0600

    Add files via upload

---
+++ Tests/controller/controller.py
@@ -0,0 +1,119 @@
+# This is the middleman between your Model and View. It will control the flow of data and handle user input.
+import RPi.GPIO as GPIO
+
+from model.model import AccelerometerModel, SensorModel, TemperatureModel
+
+
+class RocketController:
+    """
+    The RocketController class is responsible for updating the view with sensor data from the model.
+    """
+
+    def __init__(self, model, view):
+        """
+        Initializes the Controller class with a model and view object.
+
+        Args:
+            model: An instance of the Model class.
+            view: An instance of the View class.
+        """
+        self.model = model
+        self.view = view
+
+        """
+        temp sensors are on pin 7 (gpclk0) they are adressable through 1wire protocol
+        accelerometer clock is on pin 5 (gpio9) data line is on pin 3 (gpio8)
+        accelerometer 2 clock is on pin 16 (gpio4) data line is on pin 15 (gpio3)
+        barometer clock is on pin 13 (gpio2) data line is on pin 11 (gpio0) (bpm338 chip) Hex address 0x77
+        barometer 2 clock is on pin 22 (gpio22) data line is on pin 21 (gpio21)
+        """
+        sensorPinList = self.check_sensors([7, 5, 3, 16, 15, 13, 11, 22, 21])
+        # Create sensor objects
+        self.sensorObjectList = self.createSensors(sensorPinList)
+
+    def createSensors(self, sensorPinList):
+        """
+        Creates a list of SensorModel objects from a list of pins.
+
+        Args:
+            sensorPinList (list): A list of pins to which sensors are connected.
+
+        Returns:
+            list: A list of SensorModel objects.
+        """
+        # TODO: Create a list of SensorModel objects
+        #print(sensorPinList)
+        i = 0
+        # Create Temperature Sensor objects
+        TemperatureSensor1 = TemperatureModel(28, 0)#sensorPinList[i], 0)
+        TemperatureSensor2 = TemperatureModel(28, 1)#sensorPinList[i], 1)
+        TemperatureSensor3 = TemperatureModel(28, 2)#sensorPinList[i], 2)
+        TemperatureSensor4 = TemperatureModel(28, 3)#sensorPinList[i], 3)
+        i += 1
+        # Create Accelerometer Sensor object
+        AccelerometerSensor1 = AccelerometerModel(
+            #sensorPinList[i], sensorPinList[i + 1]
+        )
+        """
+        i += 2
+        AccelerometerSensor2 = AccelerometerModel(
+            sensorPinList[i], sensorPinList[i + 1]
+        )
+        i += 2
+        # Create Barometer Sensor object
+        BarometerSensor1 = SensorModel(sensorPinList[i], sensorPinList[i + 1])
+        i += 2
+        BarometerSensor2 = SensorModel(sensorPinList[i], sensorPinList[i + 1])
+        i += 2
+        """
+        # Return list of sensor objects
+        sensorList = [
+            TemperatureSensor1,
+            TemperatureSensor2,
+            TemperatureSensor3,
+            TemperatureSensor4,
+            AccelerometerSensor1
+            #AccelerometerSensor2,
+            #BarometerSensor1,
+            #BarometerSensor2,
+        ]
+        #print(sensorList)
+        return sensorList
+
+    def update_view(self):
+        """
+        Updates the view with the latest sensor data from the model.
+
+        """
+        #print(self.sensorObjectList)
+        data = ""
+        for sensor in self.sensorObjectList:
+            #print(sensor, end='')
+            data = data + str(sensor.readData()) + "\t"
+        self.view.display_data(data)
+
+    def check_sensors(self, pinList):
+        """
+        Checks the given pins for connected sensors. Returns a list of pins with connected sensors.
+
+        Returns:
+            list: A list of pins connected to sensors.
+        """
+        connected_sensors = []
+        disconnected_sensors = []
+
+        GPIO.setmode(GPIO.BCM)
+
+        for (
+            pin
+        ) in (
+            pinList
+        ):  # TODO: Check to see if all sensors output HIGH when connected, or this won't work
+            GPIO.setup(pin, GPIO.IN)
+            if GPIO.input(pin):
+                connected_sensors.append(pin)
+            else:
+                disconnected_sensors.append(pin)
+        print("Connected sensors: ", connected_sensors)
+        print("Disconnected sensors: ", disconnected_sensors)
+        return connected_sensors
---
+++ Tests/main.py
@@ -0,0 +1,41 @@
+# Import custom classes
+from model.model import RocketModel
+from view.view import RocketView
+from controller.controller import RocketController
+import csv
+import os
+
+# This conditional makes sure your code only runs when the script is executed directly
+# and not when it's imported as a module in another script.
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+
+if __name__ == "__main__":
+    # Create an instance of the Model. This is where you'll manage your sensor data
+    # and database interactions.
+    model = RocketModel()
+
+    # Create an instance of the View. The View is responsible for presenting data to the user.
+    # Since you only need console output, this can start as simple print statements.
+    view = RocketView()
+
+    # Create an instance of the Controller, passing in the model and view.
+    # The Controller will handle user inputs and update the View based on data from the Model.
+    controller = RocketController(model, view)
+
+    # You might want to have a loop here for continuous updates.
+    # If you need to perform tasks at a set interval, you can use a time.sleep() call.
+    # If the script is meant to run indefinitely, consider handling exit conditions properly.
+    try:
+        while True:
+            # This function call tells the controller to update the View with new data.
+            controller.update_view()
+
+            # If your sensors update frequently, you could add a sleep here.
+            # For example, time.sleep(1) would pause the loop for 1 second before continuing.
+            # This is important to prevent your loop from consuming too much CPU by running as fast as possible.
+    except KeyboardInterrupt:
+        # This block allows the user to stop the loop with a keyboard interrupt (Ctrl+C).
+        print("Shutting down the rocket controller.")
+        # Here you could also add any necessary clean-up code to safely terminate the application.
