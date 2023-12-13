be17a9b6bfb2bc9010aed70d81e41b66816ea3bf
Nick <huffmannp26@uww.edu>
2023-12-13 00:31:50 -0600

    ?????

--- src/view/view.py
+++ src/view/view.py
@@ -6,17 +6,8 @@ class RocketView:
 
     def display_data(self, data):
         # Print the headers
-<<<<<<< HEAD
         self.write_data(data)
         print("{:<15} {:<15} \t\t {:<15}".format("Sensor type", "Timestamp", "Sensor data"))
-=======
-        print(data)
-        print(
-            "{:<15} {:<15} \t\t {:<15}".format(
-                "Sensor type", "Timestamp", "Sensor data"
-            )
-        )
->>>>>>> 5c1abdc9c0852defc974838c4fb5025b56e5d746
         # Loop through the data and print each item
         sensors = [
             "Temp1",
@@ -38,24 +29,12 @@ class RocketView:
             i += 1
             j += 1
         while i < 7:
-<<<<<<< HEAD
             print(sensors[i] + "\t" + str(data[0]) + "\t",
               '{0:.1f}'.format(data[j]),
               '\t{0:.1f}'.format(data[j+1]),
               '\t{0:.1f}'.format(data[j+2]), end="\n")
             i+=1
             j+=3
-=======
-            print(
-                sensors[i] + "\t" + str(data[0]) + "\t",
-                "{0:.2f}".format(data[j]),
-                "\t{0:.2f}".format(data[j + 1]),
-                "\t{0:.2f}".format(data[j + 2]),
-                end="\n",
-            )
-            i += 1
-            j += 3
->>>>>>> 5c1abdc9c0852defc974838c4fb5025b56e5d746
         """
         print(sensors[i] + "\t" + str(data[0]),
               '{0:.2f}'.format(data[j]),
